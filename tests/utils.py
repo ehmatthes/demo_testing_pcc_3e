4b42e9b2d2883bd21e992c9a9aee4cd9173b64a1
Eric Matthes <ehmatthes@gmail.com>
2023-09-26 21:42:07 -0800

    Started utils, with run_command() function.

---
+++ tests/utils.py
@@ -0,0 +1,12 @@
+"""Utilities for pcc_3e tests."""
+
+from shlex import split
+import subprocess
+
+def run_command(cmd):
+    """Run a command, and return the output."""
+    cmd_parts = split(cmd)
+    result = subprocess.run(cmd_parts,
+        capture_output=True, text=True, check=True)
+    
+    return result.stdout.strip()
