7fdb72824d4a0d6d1efc332a7fc31e4661a2b78d
Eric Matthes <ehmatthes@gmail.com>
2023-09-30 11:43:58 -0800

    Resets test environment at end of test run.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 """conftest.py for tests/ directory."""
 
+from pathlib import Path
+
 import pytest
 
 import utils
@@ -21,9 +23,31 @@ def python_cmd():
 
 def pytest_sessionfinish(session, exitstatus):
     """Custom cleanup work."""
+    python_cmd = utils.get_python_cmd()
+
+    # Reset any libraries that had a different version installed
+    #   during the test run.
+    print("\n\n--- Resetting test venv ---\n")
+
+    req_txt_path = Path(__file__).parents[1] / 'requirements.txt'
+
+    cmd = f"{python_cmd} -m pip install -r {req_txt_path}"
+    output = utils.run_command(cmd)
+
+    changed_lines = [
+        line for line in output.split('\n')
+        if 'Requirement already satisfied' not in line
+    ]
+    
+    if changed_lines:
+        for line in changed_lines:
+            print(line)
+    else:
+        print("  No packages were modified.")
+
+    print("\n--- Finished resetting test venv ---\n")
 
     # Show which version of Python was used for tests.
-    python_cmd = utils.get_python_cmd()
     cmd = f"{python_cmd} --version"
     output = utils.run_command(cmd)
 
